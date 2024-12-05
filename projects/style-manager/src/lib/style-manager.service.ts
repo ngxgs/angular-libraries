b6dbc0a02e76897614dabe32b0e13d895373c629
sougabriel <gabriel.dsouzapro@gmail.com>
2024-12-05 12:40:57 -0300

    feat(style-manager): change functions returns and update javadocs

--- projects/style-manager/src/lib/style-manager.service.ts
+++ projects/style-manager/src/lib/style-manager.service.ts
@@ -4,15 +4,17 @@ import { inject, Injectable, RendererFactory2 } from '@angular/core';
  * Injectable class for managing stylesheets. Stylesheets are loaded into
  * the head element and they can be removed or changed later.
  */
-@Injectable({ providedIn: 'root' })
+@Injectable({
+    providedIn: 'root',
+})
 export class StyleManager {
-    protected readonly rendererFactory = inject(RendererFactory2);
-    protected readonly renderer = this.rendererFactory.createRenderer(null, null);
+    protected readonly rendererF2 = inject(RendererFactory2);
+    protected readonly renderer = this.rendererF2.createRenderer(null, null);
     /**
-     * Set the stylesheet with the specified key for managing later. 
-     * 
+     * Set the stylesheet with the specified key for managing later.
+     *
      * @param key Is a class for the created link element.
-     * @param href The url to the stylesheet. 
+     * @param href The url to the stylesheet.
      */
     setStyle(key: string, href: string) {
         this.getLinkElementForKey(key).setAttribute('href', href);
@@ -20,14 +22,17 @@ export class StyleManager {
 
     /**
      * Remove the stylesheet with the specified key.
-     * 
+     *
      * @param key Is a class of the existing link element to be removed.
+     * @returns True if successfully remove style or false if not.
      */
-    removeStyle(key: string) {
+    removeStyle(key: string): boolean {
         const existingLinkElement = this.getExistingLinkElementByKey(key);
         if (existingLinkElement) {
             this.renderer.removeChild(document.head, existingLinkElement);
+            return true;
         }
+        return false;
     }
 
     protected getLinkElementForKey(key: string) {
@@ -38,17 +43,14 @@ export class StyleManager {
     }
 
     protected getExistingLinkElementByKey(key: string) {
-        return document.head.querySelector(
-            `link[rel="stylesheet"].${key}`
-        );
+        return document.head.querySelector(`link[rel="stylesheet"].${key}`);
     }
 
     protected createLinkElementWithKey(key: string) {
         const linkEl = this.renderer.createElement('link');
-        this.renderer.setAttribute(linkEl, 'rel', 'stylesheet')
+        this.renderer.setAttribute(linkEl, 'rel', 'stylesheet');
         this.renderer.addClass(linkEl, `${key}`);
         this.renderer.appendChild(document.head, linkEl);
         return linkEl;
     }
-
 }
