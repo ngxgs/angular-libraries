965868eb70319f2edbaa606cfe689bfd65c1d2c6
sougabriel <gabriel.dsouzapro@gmail.com>
2024-12-05 12:38:05 -0300

    test(connection-status): add `should be true the initial online status` test implementation

--- projects/connection-status/src/lib/connection-status.service.spec.ts
+++ projects/connection-status/src/lib/connection-status.service.spec.ts
@@ -3,18 +3,18 @@ import { TestBed } from '@angular/core/testing';
 import { ConnectionStatus } from './connection-status.service';
 
 describe('ConnectionStatus', () => {
-  let service: ConnectionStatus;
+    let service: ConnectionStatus;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(ConnectionStatus);
-  });
+    beforeEach(() => {
+        TestBed.configureTestingModule({});
+        service = TestBed.inject(ConnectionStatus);
+    });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+    it('should be created the service', () => {
+        expect(service).toBeTruthy();
+    });
 
-  it('should be true', () => {
-    expect(service.isOnline()).toBeTrue();
-  });
+    it('should be true the initial online status', () => {
+        expect(service.status()).toBeTrue();
+    });
 });
