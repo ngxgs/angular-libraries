c66d55f3952b304658568c8b60c04f09ae3828a6
sougabriel <gabriel.dsouzapro@gmail.com>
2024-12-05 12:40:10 -0300

    feat(powered-storage): change functions returns and update javadocs

--- projects/powered-storage/src/lib/powered-storage.service.ts
+++ projects/powered-storage/src/lib/powered-storage.service.ts
@@ -1,115 +1,128 @@
 import { Injectable } from '@angular/core';
 
 /**
- * Inject this class for easily managing Local and Session Storage.
+ * Provides a service for easily managing Local and Session Storage.
  */
 @Injectable({
-  providedIn: 'root',
+    providedIn: 'root',
 })
-export class PoweredStorage {  
-  /**
-   * Method to store items on the storage. 
-   * 
-   * @param key The identifier of the item to be storage.
-   * @param value Anything you want to be storage.
-   * @param expiration Optional expiration date to remove the item.
-   * @param storage The storage used to set items, localStorage is default.
-   */
-  setItem(key: string, value: unknown, expiration?: Date, storage: Storage = localStorage) {
-    if (key) {
-      try {
-        const now = Date.now();
-        const today = new Date(now);
-        if (expiration && expiration > today) {
-          const item = { value, expiration }
-          storage.setItem(key.trim(), JSON.stringify(item));  
-        } else {
-          storage.setItem(key.trim(), JSON.stringify(value));
+export class PoweredStorage {
+    /**
+     * Method to store items on the storage.
+     *
+     * @param key The identifier of the item to be storage.
+     * @param value Anything you want to be storage.
+     * @param expiration Optional expiration date to remove the item. The date must be after the current date.
+     * @param storage The storage used to set items, localStorage is default.
+     * @returns True if the item was created or false if not.
+     */
+    setItem(
+        key: string,
+        value: unknown,
+        expiration?: Date,
+        storage: Storage = localStorage
+    ): boolean {
+        if (key) {
+            try {
+                const now = Date.now();
+                const today = new Date(now);
+                if (expiration && expiration > today) {
+                    const item = { value, expiration };
+                    storage.setItem(key.trim(), JSON.stringify(item));
+                } else {
+                    storage.setItem(key.trim(), JSON.stringify(value));
+                }
+                return true;
+            } catch {
+                return false;
+            }
         }
-      } catch {}
+        return false;
     }
-  }
 
-  /**
-   * Method to get item on the storage by the key. If the item has expiration and is the date defined, the item is be returned and removed. 
-   * 
-   * @param key The identifier of the value to be returned.
-   * @param storage The storage used to set items, localStorage is default.
-   * @returns The item on the storage or void if not exists. It will return null if the key provided is not valid.
-   */
-  getItem<T>(key: string, storage: Storage = localStorage): T | null {
-    if (key) {
-      try {
-        const itemString = storage.getItem(key.trim());
-        if (itemString) {
-          const now = Date.now();
-          const today = new Date(now);
-          const item = JSON.parse(itemString);
-          if ((item.expiration as Date) == today) {
-            this.removeItem(key);
-          }
-          return item;
+    /**
+     * Method to get item on the storage by the key. If the item has expiration and is the date defined, the item is be returned and removed.
+     *
+     * @param key The identifier of the value to be returned.
+     * @param storage The storage used to set items, localStorage is default.
+     * @returns The item on the storage or null if not exists. It will return null if the key provided is not valid.
+     */
+    getItem<T>(key: string, storage: Storage = localStorage): T | null {
+        if (key) {
+            try {
+                const itemString = storage.getItem(key.trim());
+                if (itemString) {
+                    const today = new Date(Date.now());
+                    const item = JSON.parse(itemString);
+                    if ((item.expiration as Date) == today) {
+                        this.removeItem(key);
+                    }
+                    return item;
+                }
+            } catch {
+                return null;
+            }
         }
-      } catch {}
+        return null;
     }
-    return null;
-  }
 
-  /**
-   * Method to remove a single item on the storage by the key.
-   * 
-   * @param key The identifier of the value to be returned.
-   * @param storage The storage used to set items, localStorage is default.
-   * @returns Item removed or void.
-   */
-  removeItem(key: string, storage: Storage = localStorage) {
-    if (key) {
-      const item = this.getItem(key, storage);
-      storage.removeItem(key.trim());
-      return item;
+    /**
+     * Method to remove a single item on the storage by the key.
+     *
+     * @param key The identifier of the value to be returned.
+     * @param storage The storage used to set items, localStorage is default.
+     * @returns True if the item was removed or false if not.
+     */
+    removeItem(key: string, storage: Storage = localStorage): boolean {
+        if (key) {
+            storage.removeItem(key.trim());
+            return true;
+        }
+        return false;
     }
-    return;
-  }
 
-  /**
-   * Method to remove all items on the storage.
-   * 
-   * @param storage The storage used to set items, localStorage is default.
-   * @returns All items removed.
-   */
-  removeAllItems(storage: Storage = localStorage) {
-    const items = this.getAllItens();
-    storage.clear();
-    return items;
-  }
+    /**
+     * Method to remove all items on the storage.
+     *
+     * @param storage The storage used to set items, localStorage is default.
+     * @returns True if successfully remove all items or false if not.
+     */
+    removeAllItems(storage: Storage = localStorage) {
+        try {
+            storage.clear();
+            return true;
+        } catch {
+            return false;
+        }
+    }
 
-  /**
-   * Method to get all items on the storage. 
-   * 
-   * @param storage The storage used to set items, localStorage is default.
-   * @returns All items on the storage.
-   */
-  getAllItens(storage: Storage = localStorage) {
-    let values: ItemStorage[] = [];
-    let keys = Object.keys(localStorage);
+    /**
+     * Method to get all items on the storage.
+     *
+     * @param storage The storage used to set items, localStorage is default.
+     * @returns All items on the storage.
+     */
+    getAllItens(storage: Storage = localStorage) {
+        let values: ItemStorage[] = [];
+        let keys = Object.keys(localStorage);
 
-    keys.forEach((key) => {
-      const ItemStorage = storage.getItem(key);
-      if (ItemStorage) {
-        const item: ItemStorage = { key: key, value: ItemStorage };
-        values.push(item);
-      }
-    });
+        keys.forEach((key) => {
+            const ItemStorage = storage.getItem(key);
+            if (ItemStorage) {
+                const item: ItemStorage = { key: key, value: ItemStorage };
+                values.push(item);
+            }
+        });
 
-    return values;
-  }
+        return values;
+    }
 }
 
 /**
  * Item class shape.
  */
 export interface ItemStorage {
-  key: string;
-  value: any;
-  expiration?: Date;
+    key: string;
+    value: unknown;
+    expiration?: Date;
 }
